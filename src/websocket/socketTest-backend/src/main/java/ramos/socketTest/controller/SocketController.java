2f20ac2d2fd21740aaa8c59a16f59acae05cd99e
alanhakhyeonsong <songs4805@naver.com>
2022-05-21 13:29:10 +0900

    Frontend Web Socket test using Vue.js

--- src/websocket/socketTest-backend/src/main/java/ramos/socketTest/controller/SocketController.java
+++ src/websocket/socketTest-backend/src/main/java/ramos/socketTest/controller/SocketController.java
@@ -2,11 +2,11 @@ package ramos.socketTest.controller;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import ramos.socketTest.dto.MessageRequest;
 
-@RestController
+@Controller
 public class SocketController {
 
     @MessageMapping("/receive")
--- src/websocket/socketTest-backend/src/main/java/ramos/socketTest/dto/MessageRequest.java
+++ src/websocket/socketTest-backend/src/main/java/ramos/socketTest/dto/MessageRequest.java
@@ -2,9 +2,11 @@ package ramos.socketTest.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class MessageRequest {
     private String userName;
     private String message;
